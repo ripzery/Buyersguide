6a0ca2ca7fcba483164c3323b96c6c51a0637b63
Phuchit@Dell <ripzery@gmail.com>
2015-07-29 13:13:43 +0700

    fix color of progress last release

--- controllers/app.js
+++ controllers/app.js
@@ -1,14 +1,34 @@
 var app = angular.module('Droidaday', ['ngMaterial']);
 
 app.config(function($mdThemingProvider) {
+
+    $mdThemingProvider.definePalette('progressPalette',{
+        '50': 'ffebee',
+        '100': 'ffcdd2',
+        '200': 'ef9a9a',
+        '300': 'e57373',
+        '400': 'ef5350',
+        '500': 'f44336',
+        '600': 'e53935',
+        '700': 'd32f2f',
+        '800': 'c62828',
+        '900': 'b71c1c',
+        'A100': 'ff8a80',
+        'A200': 'ff5252',
+        'A400': 'ff1744',
+        'A700': 'd50000',
+        'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                            // on this palette should be dark or light
+        'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+            '200', '300', '400', 'A100'],
+        'contrastLightColors': undefined    // could also specify this if default was 'dark'
+    });
+
     $mdThemingProvider.theme('default')
-        .primaryPalette('light-green')
-        .accentPalette('teal',{
-            'default': '500',
-            'hue-1' : '100',
-            'hue-2' : '200',
-            'hue-3' : '300'
-        });
+        .primaryPalette('green')
+        .accentPalette('amber')
+        .warnPalette('red')
+    ;
 });
 
 app.directive('guideCard',function(){
@@ -20,10 +40,14 @@ app.directive('guideCard',function(){
             model : '=model'
         },
         controller: function ($scope) {
+
+            $scope.lastReleasePercent = $scope.model.announceCount / $scope.model.announceAverage * 100;
+            console.log($scope.lastReleasePercent);
+
             var percentColors = [
-                {pct: 0.0, color: {r: 0xff, g: 0x00, b: 0}},
-                {pct: 0.5, color: {r: 0xff, g: 0xff, b: 0}},
-                {pct: 1.0, color: {r: 0x00, g: 0xff, b: 0}}];
+                {pct: 0.0, color: {r: 0xf4, g: 0x43, b: 0x36}},
+                {pct: 0.5, color: {r: 0xff, g: 0xc1, b: 0x07}},
+                {pct: 1.0, color: {r: 0x4c, g: 0xaf, b: 0x50}}];
 
 
             $scope.getColorForPercentage = function (pct) {
@@ -43,7 +67,6 @@ app.directive('guideCard',function(){
                     g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
                     b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
                 };
-                console.log('rgb(' + [color.r, color.g, color.b].join(',') + ')');
                 return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
                 // or output as hex if preferred
             };
