8b9e3346aa8af3a1ad0deeab47cb7d9b34d41ee9
phuchit <ripzery@gmail.com>
2015-07-29 09:18:43 +0700

    try to change color

--- controllers/BuyersGuideController.js
+++ controllers/BuyersGuideController.js
@@ -21,7 +21,7 @@ app.controller('BuyersGuideController',['$scope','$http',function($scope,$http){
                     angular.forEach(value.announceHistory, function(value,innerKey){
                         var date = new Date(value.date);
                         $scope.models[outerKey].announceHistory[innerKey].date = $scope.allMonthes[date.getMonth()] + " " + date.getUTCFullYear();
-                        console.log($scope.models[outerKey].announceHistory[innerKey].date);
+                        //console.log($scope.models[outerKey].announceHistory[innerKey].date);
                     });
                 });
             });
@@ -31,6 +31,7 @@ app.controller('BuyersGuideController',['$scope','$http',function($scope,$http){
         {pct: 0.0, color: {r: 0xff, g: 0x00, b: 0}},
         {pct: 0.5, color: {r: 0xff, g: 0xff, b: 0}},
         {pct: 1.0, color: {r: 0x00, g: 0xff, b: 0}}];
+
 
     $scope.getColorForPercentage = function (pct) {
         for (var i = 1; i < percentColors.length - 1; i++) {
@@ -49,9 +50,10 @@ app.controller('BuyersGuideController',['$scope','$http',function($scope,$http){
             g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
             b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
         };
+        console.log('rgb(' + [color.r, color.g, color.b].join(',') + ')');
         return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
         // or output as hex if preferred
-    }
+    };
 
     $scope.init();
 }]);
