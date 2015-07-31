80c75448d939b0cec6ff9ac50514dd9acb19a9e5
phuchit <ripzery@gmail.com>
2015-07-31 14:59:14 +0700

    change color of text and progressbar

--- controllers/app.js
+++ controllers/app.js
@@ -1,5 +1,8 @@
 var app = angular.module('Droidaday', ['ngMaterial']);
 
+var _theme;
+var _palettes;
+
 app.config(function($mdThemingProvider) {
 
     $mdThemingProvider.definePalette('progressPalette',{
@@ -27,8 +30,10 @@ app.config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
         .primaryPalette('green')
         .accentPalette('amber')
-        .warnPalette('red')
-    ;
+        .warnPalette('red');
+
+    _theme = $mdThemingProvider.theme();
+    _palettes = $mdThemingProvider._PALETTES;
 });
 
 app.directive('guideCard',function(){
@@ -43,6 +48,30 @@ app.directive('guideCard',function(){
 
             $scope.lastReleasePercent = $scope.model.announceCount / $scope.model.announceAverage * 100;
             console.log($scope.lastReleasePercent);
+
+            $scope.findTextColor = function () {
+                if ($scope.lastReleasePercent >= 90) {
+                    $scope.textColor = 'warn.hue-2';
+                } else if ($scope.lastReleasePercent >= 80) {
+                    $scope.textColor = 'warn.500';
+                } else if ($scope.lastReleasePercent >= 70) {
+                    $scope.textColor = 'warn.hue-1';
+                } else if ($scope.lastReleasePercent >= 60) {
+                    $scope.textColor = 'accent.hue-3';
+                } else if ($scope.lastReleasePercent >= 50) {
+                    $scope.textColor = 'accent.hue-2';
+                } else if ($scope.lastReleasePercent >= 40) {
+                    $scope.textColor = 'primary.hue-1';
+                } else if ($scope.lastReleasePercent >= 30) {
+                    $scope.textColor = 'primary.500';
+                } else if ($scope.lastReleasePercent >= 20) {
+                    $scope.textColor = 'primary.hue-2';
+                } else {
+                    $scope.textColor = 'primary.hue-3';
+                }
+            };
+
+            $scope.findTextColor();
 
             var percentColors = [
                 {pct: 0.0, color: {r: 0xf4, g: 0x43, b: 0x36}},
@@ -73,3 +102,42 @@ app.directive('guideCard',function(){
         }
     };
 });
+
+app.directive('mdStyleColor',
+    function ($mdColorPalette) {
+        return {
+            restrict: 'A',
+            scope: {mdStyleColor: '='},
+            link: function (scope, element, attrs) {
+                for (var p in scope.mdStyleColor) {
+                    if (scope.mdStyleColor.hasOwnProperty(p)) {
+
+                        var themeColors = _theme.colors;
+
+                        var split = (scope.mdStyleColor[p] || '').split('.');
+                        if (split.length < 2) split.unshift('primary');
+
+                        var hueR = split[1] || 'hue-1';    // 'hue-1'
+                        var colorR = split[0] || 'primary';  // 'warn'
+
+                        // Absolute color: 'orange'
+                        var colorA = themeColors[colorR] ?
+                            themeColors[colorR].name : colorR;
+
+                        // Absolute Hue: '500'
+                        var hueA =
+                            themeColors[colorR] ?
+                            themeColors[colorR].hues[hueR] || hueR :
+                                hueR;
+
+                        var colorValue = _palettes[colorA][hueA] ?
+                            _palettes[colorA][hueA].value :
+                            _palettes[colorA]['500'].value;
+
+                        element.css(p, 'rgb(' + colorValue.join(',') + ')');
+
+                    }
+                }
+            }
+        }
+    });
